ff777851365de2469f52515d548b633c68887f78
gatoMena <od_mf_97@hotmail.com>
2018-08-03 18:45:19 -0500

    ingreso de la url

---
+++ BuiltWith/code.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+#main
+print "+----------------------------------+"
+print "|             BuiltWith            |"
+print "+----------------------------------+"
+
+url = raw_input("Ingrese la url: ")
+print "* Target: "+ url+"\n"
+os.system('curl https://builtwith.com/'+ url+ '>temp' )
+
+f = open ('temp.txt','r')
+info = f.read()
+a = info.find("")
+
+f.close()
+os.system('rm temp.txt');
