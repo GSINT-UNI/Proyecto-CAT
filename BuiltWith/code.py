a78bccaf9a848fd13ba003b583ac5245ec638557
gatoMena <od_mf_97@hotmail.com>
2018-08-03 20:45:58 -0500

    extracción de herramientas

--- BuiltWith/code.py
+++ BuiltWith/code.py
@@ -1,5 +1,10 @@
 import os
 import time
+import re
+
+#elimina etiquetas html
+def strip_tags(value):
+    return re.sub(r'<[^>]*?>', '', value)
 
 #main
 print "+----------------------------------+"
@@ -12,9 +17,22 @@ os.system('curl https://builtwith.com/'+ url+ '>temp.txt' )
 
 f = open ('temp.txt','r')
 info = f.read()
-a = info.find("")
-
-
-
+a=0
+b=0
+s = ""
+while a!=-1 and b!=-1:
+    a = info.find("<h6 class=\"card-title\">",a+1)
+    b = info.find("<",a+1)
+    for i in range(a+23,b):
+        s = s +  info[i]
+    s = s+":\n"
+    c = info.find("<h2",b+1)
+    c = info.find("<a",c+1)
+    c = info.find(">",c+1)
+    d = info.find("<",c+1)
+    for i in range(c,d):
+        s = s +  info[i]
+    s= s+".\n"
+print s
 f.close()
 os.system('rm temp.txt');
