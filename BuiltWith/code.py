410223122e2e53e50aea5ee5816b11b2bf8063df
gatoMena <od_mf_97@hotmail.com>
2018-08-03 19:01:33 -0500

    corrección del archivo temporal

--- BuiltWith/code.py
+++ BuiltWith/code.py
@@ -8,11 +8,13 @@ print "+----------------------------------+"
 
 url = raw_input("Ingrese la url: ")
 print "* Target: "+ url+"\n"
-os.system('curl https://builtwith.com/'+ url+ '>temp' )
+os.system('curl https://builtwith.com/'+ url+ '>temp.txt' )
 
 f = open ('temp.txt','r')
 info = f.read()
 a = info.find("")
+
+
 
 f.close()
 os.system('rm temp.txt');
