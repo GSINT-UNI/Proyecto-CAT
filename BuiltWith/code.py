6cee621ef8349b0b4acf15edc17dbf4beb066fb5
gatoMena <od_mf_97@hotmail.com>
2018-08-04 01:31:20 -0500

    herramientas con sus especificaciones

--- BuiltWith/code.py
+++ BuiltWith/code.py
@@ -1,38 +1,52 @@
 import os
 import time
-import re
-
-#elimina etiquetas html
-def strip_tags(value):
-    return re.sub(r'<[^>]*?>', '', value)
 
 #main
-print "+----------------------------------+"
-print "|             BuiltWith            |"
-print "+----------------------------------+"
+print("+----------------------------------+")
+print("|             BuiltWith            |")
+print("+----------------------------------+")
 
-url = raw_input("Ingrese la url: ")
-print "* Target: "+ url+"\n"
-os.system('curl https://builtwith.com/'+ url+ '>temp.txt' )
+url = input("Ingrese la url: ")
+print("* Target: "+ url+"\n")
+os.system('curl https://builtwith.com/'+url+'>temp.txt')
 
 f = open ('temp.txt','r')
 info = f.read()
-a=0
-b=0
-s = ""
-while a!=-1 and b!=-1:
-    a = info.find("<h6 class=\"card-title\">",a+1)
-    b = info.find("<",a+1)
-    for i in range(a+23,b):
-        s = s +  info[i]
-    s = s+":\n"
-    c = info.find("<h2",b+1)
-    c = info.find("<a",c+1)
-    c = info.find(">",c+1)
-    d = info.find("<",c+1)
-    for i in range(c,d):
-        s = s +  info[i]
-    s= s+".\n"
-print s
+ini2=0
+fin2=0
+ini=0
+fin=0
+herramienta=""
+print ("\n")
+print("##########################################")
+print("#       HERRAMIENTAS ENCONTRADAS         #")
+print("##########################################")
+
+while ini != -1:
+    ini=ini+1
+    ini=info.find("<h6 class=\"card-title\">",ini)
+    limite=info.find("<h6 class=\"card-title\">",ini+1)
+    fin=info.find("<",ini+1)
+    for a in range(ini+len("<h6 class=\"card-title\">"),fin):
+        herramienta=herramienta+info[a]
+    if(herramienta!=""):
+        print("# "+herramienta)
+    herramienta=""
+    tool=""
+    ini2=ini
+    if limite==-1:
+        limite=len(info)
+    while ini2<limite:
+        ini2=ini2+1
+        ini2=info.find("<a class=\"text-dark\" href=\"",ini2,limite)
+        ini2=info.find(">",ini2,limite)
+        fin2=info.find("<",ini2,limite)
+        for a in range(ini2+1,fin2):
+            tool=tool+info[a]
+        if(tool!=""):
+            print("   *"+tool)
+        if (ini2==-1):
+            break
+
 f.close()
 os.system('rm temp.txt');
